d2e6cc5e181fae1cb9c8d736e7d6e19f33f2dee0
derek <derekchili@gmail.com>
2023-04-10 13:04:44 -0700

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-// AHHHHHHHHHHHH!!!!!!!
+// this sends a get response to the API (google) and retrieves the response data
+response = requests.get(endpoint, params=params).json();
+
+// retrieves items in the arrayfrom response json
+items = response['items'];
+
+// need a loop for first five items and display them into the card
+
+
+
+$(document).ready(function() {
+    $('#author-search').click(function() {
+        
+    })
+})
+
+$.get(endpoint, params, function(response) {
+    var items = response.items;
+
+    
+    for (var i = 0; i < Math.min(items.lenght, 5); i++) {
+        var title = items[i].volumeInfo.title;
+        console.log(title) 
+
+    }
+})
