40468a11df13a8e43987b18ee5d919535990baae
Citlalli091 <120295784+Citlalli091@users.noreply.github.com>
2023-04-10 12:17:50 -0700

    API plugged in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 // AHHHHHHHHHHHH!!!!!!!
+var googUrl = 'https://www.googleapis.com/books/v1/volumes?q=search+terms&key=AIzaSyD_J1_2HDf8XZGF7p11aeX7W_ICizZspas';
+
+var libUrl = 'https://api.nytimes.com/svc/books/v3';
+
+function grabApi(googUrl) {
+    fetch(googUrl)
+        .then(function(response) {
+            console.log(response);
+            return response.json();
+ });
+}
+grabApi(googUrl);
+
+function grabApi2(libUrl) {
+    fetch(libUrl)
+        .then(function(response) {
+            console.log(response);
+            return response.json();
+ });
+}
+grabApi2(libUrl);
