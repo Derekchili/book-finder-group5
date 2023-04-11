d22f3de6f9144fc00439c389134b39c6ee906201
Ryan Pratt <ryan.k.pratt@gmail.com>
2023-04-10 20:33:04 -0700

    Added api data gathering functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,31 @@ var libUrl = 'https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=9
 console.log(googUrl);
 console.log(libUrl);
 
-function grabApi(googUrl) {
-    fetch(googUrl)
-        .then(function(response) {
-            console.log(response);
-            return response.json();
- })
-    .then(function (data) {
-        console.log('Fetch Response \n----------');
-        console.log(data);
- })
-}
-grabApi(googUrl);
+// function grabApi(googUrl) {
+//     fetch(googUrl)
+//         .then(function(response) {
+//             console.log(response);
+//             return response.json();
+//  })
+//     .then(function (data) {
+//         console.log('Fetch Response \n----------');
+//         console.log(data);
+//  })
+// }
+// grabApi(googUrl);
 
-function grabApi2(libUrl) {
-    fetch(libUrl)
-        .then(function(response) {
-            console.log(response);
-            return response.json();
- })
- .then(function (data) {
-    console.log('Fetch Response \n----------');
-    console.log(data);
-})
-}
-grabApi2(libUrl);
+// function grabApi2(libUrl) {
+//     fetch(libUrl)
+//         .then(function(response) {
+//             console.log(response);
+//             return response.json();
+//  })
+//  .then(function (data) {
+//     console.log('Fetch Response \n----------');
+//     console.log(data);
+// })
+// }
+// grabApi2(libUrl);
 
 
 // this sends a get response to the API (google) and retrieves the response data
@@ -58,3 +58,80 @@ grabApi2(libUrl);
 
 //     }
 // })
+$(function () {
+    var auth = $("#author-search");
+    auth.on("change", authSearch);
+    var genre = $("#genre-search");
+    genre.on("change", genreSearch);
+    var trend = $("#dropdown");
+    trend.on("change", trendSearch);
+    retrieveGenres();
+});
+
+function authSearch() {
+    alert("test " + $(this).val());
+    callGoogle($(this).val());
+}
+
+function genreSearch() {
+    alert("test " + $(this).val());
+    callNYT($(this).val());
+}
+
+function trendSearch() {
+    alert("test " + $(this).val());
+}
+
+function callGoogle(searchWords) {
+    searchWords = searchWords.replace(/\s+/g, '+').toLowerCase();
+    $.ajax({
+        type: "GET",
+        url: "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + searchWords + "&key=AIzaSyD_J1_2HDf8XZGF7p11aeX7W_ICizZspas",
+        dataType: "json",
+        success: function (result, status, xhr) {
+            alert("result " + result);
+        },
+        error: function (xhr, status, error) {
+            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
+        }
+    })
+}
+
+function callNYT(searchGenres) {
+    searchGenres = searchGenres.replace(/\s+/g, '-').toLowerCase();
+    console.log(searchGenres);
+    $.ajax({
+        type: "GET",
+        url: "https://api.nytimes.com/svc/books/v3/lists/current/" + searchGenres + ".json?api-key=9p5nzFHMFVgj5PbY4jWUFUAEz1POGKRa",
+        dataType: "json",
+        success: function (result, status, xhr) {
+            alert("result " + result);
+        },
+        error: function (xhr, status, error) {
+            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
+        }
+    })
+}
+
+function retrieveGenres() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=9p5nzFHMFVgj5PbY4jWUFUAEz1POGKRa",
+        dataType: "json",
+        success: function (result, status, xhr) {
+            alert("result " + result);
+            var genreList = $("#dropdown");
+            var genres = result["results"]
+            genres.forEach(genre => {
+                genreList.append(`<option value="${genre["list_name_encoded"]}">
+                            ${genre["list_name_encoded"]}
+                    </option>`
+                        );
+                 
+            });
+        },
+        error: function (xhr, status, error) {
+            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
+        }
+    })
+}
