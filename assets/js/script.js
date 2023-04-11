e3c014db7cd39ef180762fef7307f32a217d8093
Citlalli091 <120295784+Citlalli091@users.noreply.github.com>
2023-04-11 08:17:09 -0700

    branch switch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 // AHHHHHHHHHHHH!!!!!!!
 var googUrl = 'https://www.googleapis.com/books/v1/volumes?q=search+terms&key=AIzaSyD_J1_2HDf8XZGF7p11aeX7W_ICizZspas';
 
-var libUrl = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=9p5nzFHMFVgj5PbY4jWUFUAEz1POGKRa';
+var libUrl = 'https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=9p5nzFHMFVgj5PbY4jWUFUAEz1POGKRa';
+
+console.log(googUrl);
+console.log(libUrl);
 
 function grabApi(googUrl) {
     fetch(googUrl)
@@ -20,3 +23,32 @@ function grabApi2(libUrl) {
  });
 }
 grabApi2(libUrl);
+
+var
+var
+// this sends a get response to the API (google) and retrieves the response data
+// response = requests.get(endpoint, params=params).json();
+
+// // retrieves items in the arrayfrom response json
+// items = response['items'];
+
+// // need a loop for first five items and display them into the card
+
+
+
+// $(document).ready(function() {
+//     $('#author-search').click(function() {
+        
+//     })
+// })
+
+// $.get(endpoint, params, function(response) {
+//     var items = response.items;
+
+    
+//     for (var i = 0; i < Math.min(items.lenght, 5); i++) {
+//         var title = items[i].volumeInfo.title;
+//         console.log(title) 
+
+//     }
+// })
