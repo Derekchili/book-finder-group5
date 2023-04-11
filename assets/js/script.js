617697ab78cacefe53458384aa058d2c10fe0e74
derek <derekchili@gmail.com>
2023-04-11 11:52:29 -0700

    updated functions for top trending and htnl to reflect

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,7 @@ function grabApi2(nytUrl) {
  .then(function (data) {
     console.log('Fetch Response \n----------');
     console.log(data);
+
 // we have to figure out how to get this info from the array bookList into the html element #trending maybe turn it into a button with some sort of list that diplays, I also got it to display images of the book too.
     var books = data.results.books;
     var bookList = [];
@@ -42,29 +43,49 @@ function grabApi2(nytUrl) {
         bookList.push(bookInfo);
     }
     console.log(bookList);
-
-    // $('#trending').text(bookList.join('\n'));
 })
-
 }
 grabApi2(nytUrl);
 
-// // trying to create a function to grab the results from the bookList array and display it in a list on the page!?
-// function renderBtn(){
-//     if (bookList.length > 0) {
-//       bookInfo(bookList[0])
-//       for (let i = 0; i < bookList.length; i++) {
-//         $(".trending-list").append(`<li><button class='listItem'>${bookListArr[i]}</button></li>`);
+function callNYT(searchTrending) {
+    searchTrending = searchTredning.replace(/\s+/g, '-').toLowerCase();
+    console.log(searchTrending);
+    $.ajax({
+        type: "GET",
+        url: "https://api.nytimes.com/svc/books/v3/lists/current/" + searchTrending + ".json?api-key=9p5nzFHMFVgj5PbY4jWUFUAEz1POGKRa",
+        dataType: "json",
+        success: function (result) {
+            console.log("result " + result);
+        },
         
-//       }
-//     }
-//     $('.listItem').on('click', function (event){
-//       var clickTrending = $(event.target).text();
-//       bookInfo(clickTrending);
-//       console.log(clickTrending);
-//     });
-//   }
-// renderBtn();
+        
+    });
+}
+
+
+function retrieveTrending() {
+    $.ajax({
+        type: "GET",
+        url: "https://api.nytimes.com/svc/books/v3/lists/current.json?api-key=9p5nzFHMFVgj5PbY4jWUFUAEz1POGKRa",
+        dataType: "json",
+        success: function (result) {
+            alert("result " + result);
+            var topTrendList = $("#trending");
+            var trending = result["results"]
+            trending.forEach(trending => {
+                topTrendList.append(`<option class="${trending["list_current_encoded"]}">
+                ${trending["list_current_encoded"]}
+        </option>`
+                        );
+                        bookList = result['results'];
+                        console.log(bookList, 'bookList');
+            });
+        },
+    
+    }  
+    )
+    }
+
   
 
 
@@ -88,46 +109,6 @@ if (typeof(Storage) !== 'undefined') {
 
 
 
-// // we have our favorites array we should be able to use it and store it into the favorite html where it displays in a card, or list somehow?
-// $(document).ready(function() {
-//     $('.favorites-btn').on('click', function(event) {
-//     event.preventDefault(); 
-    
-//     var itemId = $(this).data('id');
-
-//     var index = favorites.indexOf(itemId);
-//     if (index === -1) {
-// // checking to see if item is already in favorites, if not then we'll add it the array
-//     favorites.push(itemId);
-//     localStorage.setItem('favorites', JSON.stringify(favorites));
-//     $(this).addClass('active');
-//     }
-//     else {
-//     favorites.splice(index, 1);
-//     localStorage.setItem('favorites', JSON.stringify(favorites));
-//     $(this).removeClass('active');
-//     }
-// });
-// });
-
-// // with the link to favorites page with a class and using a method chaining for this function
-// $(document).ready(function() {
-//     $('.favorites-link').on('click', function(event) {
-//     event.preventDefault();
-
-//     var storedFavorites = localStorage.getItem('favorites');
-//     if (storedFavorites) {
-//         var favorites = JSON.parse(storedFavorites);
-
-//         var $ul = $('<ul>');
-//         favorites.forEach(function(itemId) {
-//             var $li = $('<li>').text(itemId);
-//             $ul.append($li);
-//         });
-
-
-//     }
-// })
 $(function () {
     var auth = $("#author-search");
     auth.on("change", authSearch);
@@ -202,3 +183,98 @@ function retrieveGenres() {
     }  
     )
     }
+
+
+// // trying to create a function to grab the results from the bookList array and display it in a list on the page!?
+// function renderBtn(){
+//     if (bookList.length > 0) {
+//       bookInfo(bookList[0])
+//       for (let i = 0; i < bookList.length; i++) {
+//         $(".trending-list").append(`<li><button class='listItem'>${bookListArr[i]}</button></li>`);
+        
+//       }
+//     }
+//     $('.listItem').on('click', function (event){
+//       var clickTrending = $(event.target).text();
+//       bookInfo(clickTrending);
+//       console.log(clickTrending);
+//     });
+//   }
+// renderBtn();
+
+// // we have our favorites array we should be able to use it and store it into the favorite html where it displays in a card, or list somehow?
+// $(document).ready(function() {
+//     $('.favorites-btn').on('click', function(event) {
+//     event.preventDefault(); 
+    
+//     var itemId = $(this).data('id');
+
+//     var index = favorites.indexOf(itemId);
+//     if (index === -1) {
+// // checking to see if item is already in favorites, if not then we'll add it the array
+//     favorites.push(itemId);
+//     localStorage.setItem('favorites', JSON.stringify(favorites));
+//     $(this).addClass('active');
+//     }
+//     else {
+//     favorites.splice(index, 1);
+//     localStorage.setItem('favorites', JSON.stringify(favorites));
+//     $(this).removeClass('active');
+//     }
+// });
+// });
+
+// // with the link to favorites page with a class and using a method chaining for this function
+// $(document).ready(function() {
+//     $('.favorites-link').on('click', function(event) {
+//     event.preventDefault();
+
+//     var storedFavorites = localStorage.getItem('favorites');
+//     if (storedFavorites) {
+//         var favorites = JSON.parse(storedFavorites);
+
+//         var $ul = $('<ul>');
+//         favorites.forEach(function(itemId) {
+//             var $li = $('<li>').text(itemId);
+//             $ul.append($li);
+//         });
+
+
+//     }
+// })
+
+// function retrieveTrending() {
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "https://api.nytimes.com/svc/books/v3/lists/current.json?api-key=9p5nzFHMFVgj5PbY4jWUFUAEz1POGKRa",
+    //         dataType: "json",
+    //         success: function (result) {
+    //             alert("result " + result);
+    //             var topTrendList = $("#trending");
+    //             var trending = result["results"]
+                
+    //             bookList = trending.map(book => {
+    //                 return {
+    //                     title: book['display_name'],
+    //                     author: book['list-name'],
+    //                     imgUrl: book['list-image'],
+    //                 };
+    //             });
+    
+    //             for(var i = 0; i < bookList.length; i++) {
+    //                 var book = bookList[i];
+    //                 var button = $(`<button class="book-button">${book.title}</button>`);
+    
+    //                 button.click(function() {
+    //                     var card = ("#card");
+    //                     card.empty();
+    //                     card.append(`<img src="${book.imgUrl}" class="card-img-top">`);
+    //                     card.append(`<div class="card-body"><h5 class="card-title">${book.title}</h5><p class="card-text">${book.author}</p></div>`);
+    //                 });
+    //                 topTrendList.append(button);
+    //             }
+    //         },
+            
+    //     });
+    // }
+    
